12593cf70180e18fef46ded9a38e25d05c4cd0e1
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-03-18 23:04:27 +0800

    update

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Webpack多页面打包分析
-
-Webpack一般配合MVC(react、Vue)框架才能把优势发挥出来，但MCV又专注于SPA页面，显得多页面打包时变得复杂起来。
+# Webpack多页面打包分析，不包含MVC框架的多页面打包
 
 - ``npm install`` 安装
-- ``npm run dev`` 开发模式,访问localhost:3000``
-- ``npm run build`` 
+- ``npm run dev`` 开发模式,访问 localhost:3000
+- ``npm run build``
--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.js');
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("./webpack.config.js");
 
 for (var i in config.entry) {
-	config.entry[i].unshift(
-		"webpack-dev-server/client?http://localhost:3000/",
-		"webpack/hot/only-dev-server"
-	);
+  config.entry[i].unshift(
+    "webpack-dev-server/client?http://localhost:3000/",
+    "webpack/hot/only-dev-server"
+  );
 }
 config.module.loaders.unshift({
-    test: /\.jsx?$/,
-    loader: 'react-hot',
-    exclude: /node_modules/
+  test: /\.jsx?$/,
+  loader: "react-hot",
+  exclude: /node_modules/
 });
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 new WebpackDevServer(webpack(config), {
-	publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    stats: { colors: true }
-}).listen(3000, 'localhost', function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('server start');
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  stats: { colors: true }
+}).listen(3000, "localhost", function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log("server start");
 });
