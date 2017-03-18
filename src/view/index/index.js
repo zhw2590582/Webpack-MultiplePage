e320d531f07fb3b9453483dd6ad839f1a5535bd7
laozhaochaguan@hotmail.com <laozhaochaguan@gmail.com>
2017-03-18 22:59:59 +0800

    update

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,2 +1,10 @@
-require("../../style/commonsCss01.scss")
-console.log('pageIndex');
+require("../../style/commonsCss01")
+
+$('body').addClass('test');
+
+const test233 = "test233";
+
+console.log(test233);
+
+[1,2,3].map(n => n + 1);
+[1, 2, 3].map(n => n ** 2);
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
-var path = require('path');
-var glob = require('glob');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var node_dir = path.join(__dirname, './node_modules/');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+let path = require("path");
+let glob = require("glob");
+let webpack = require("webpack");
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+let CopyWebpackPlugin = require("copy-webpack-plugin");
+let HtmlWebpackPlugin = require("html-webpack-plugin");
 
-var isProduction = process.env.NODE_ENV === 'production';
+let node_dir = path.join(__dirname, "./node_modules/");
+let output_dir = path.join(__dirname, "./dist");
 
-var getEntry = function(globPath, pathDir) {
-  var entries = {
-    vendor: []
+//是否开发模式
+let isProduction = process.env.NODE_ENV === "production";
+
+//获取三层页面路径
+let getEntry = function(globPath, pathDir) {
+  let entries = {
+    vendor: ["jquery"]
   };
   glob.sync(globPath).forEach(function(entry) {
-    var pathname = entry.replace(pathDir, '').split('.')[0]
+    let pathname = entry.replace(pathDir, "").split(".")[0];
     entries[pathname] = entry;
   });
   return entries;
 };
-var entries = getEntry('./src/view/**/**/*.js', './src/view/');
-var chunks = Object.keys(entries);
+let entries = getEntry("./src/view/**/**/**/*.js", "./src/view/");
+let chunks = Object.keys(entries);
 
-var HtmlPlugin = [];
+//生成HTML文件
+let HtmlPlugin = [];
 chunks.forEach(function(pathname) {
-  if (pathname === 'vendor') return;
-  var conf = {
-    title: 'My App',
-    filename: (pathname == 'index/index' ? 'index.html' : 'view/' + pathname + '.html'),
-    template: entries[pathname].split('.js')[0] + '.html',
-    inject: 'body',
-    chunks: ['vendor', pathname],
+  if (pathname === "vendor") return;
+  let chunksName = pathname.substr(0, pathname.lastIndexOf("/"));
+  let conf = {
+    title: "My App",
+    filename: pathname == "index/index"
+      ? "index.html"
+      : "view/" + chunksName + "/index.html",
+    template: entries[pathname].split(".js")[0] + ".html",
+    inject: "body",
+    favicon: "./src/img/favicon.ico",
+    chunks: ["vendor", pathname],
     hash: false,
     minify: {
       removeComments: true,
@@ -39,63 +49,97 @@ chunks.forEach(function(pathname) {
 });
 
 module.exports = {
+  //入口
   entry: entries,
+  //出口
   output: {
-    path: path.join(__dirname, './dist'),
-    filename: isProduction ? 'js/[name].[hash:8].js' : 'js/[name].js',
-    publicPath: '/',
-    chunkFilename: 'chunk/[name].chunk.js'
+    path: output_dir,
+    filename: isProduction ? "js/[name].[hash:8].js" : "js/[name].js",
+    publicPath: "/",
+    chunkFilename: "chunk/[name].chunk.js"
   },
+  //loaders
   module: {
-    noParse: [],
-    loaders: [{
-      test: /\.jsx?$/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react']
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ["es2015"]
+        },
+        include: [path.resolve(__dirname, "../")]
       },
-      exclude: node_dir
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css')
-    }, {
-      test: /\.less$/,
-      loader: ExtractTextPlugin.extract('style', 'css!less')
-    }, {
-      test: /\.(png|jpe?g|gif)$/,
-      loader: 'url?limit=8192&name=img/[hash:8].[ext]'
-    }, {
-      test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: 'file?limit=10000&name=fonts/[hash:8].[ext]'
-    }, {
-      test: /\.scss$/,
-      loader: ExtractTextPlugin.extract('style', 'css!sass')
-    }]
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader"
+        })
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        loader: "url-loader?limit=8192&name=img/[hash:8].[ext]"
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "file-loader?limit=10000&name=fonts/[hash:8].[ext]"
+      }
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.json'],
+    extensions: [".js", ".json", ".scss", ".css"],
     alias: {
       mod: node_dir
     }
   },
   plugins: [
+    //公共文件打包
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: isProduction ? 'js/vendor.[hash:8].js' : 'js/vendor.js',
+      name: "vendor",
+      filename: isProduction ? "js/vendor.[hash:8].js" : "js/vendor.js",
       minChunks: 3
     }),
-    new ExtractTextPlugin(isProduction ? 'css/[name].[hash:8].css' : 'css/[name].css'),
+    //提取css
+    new ExtractTextPlugin(
+      isProduction ? "css/[name].[hash:8].css" : "css/[name].css"
+    ),
+    //复制public目录
+    new CopyWebpackPlugin([
+      {
+        from: __dirname + "/src/public",
+        to: output_dir + "/public"
+      }
+    ]),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
+    //混淆js
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings: false
+        warnings: false,
+        // drop_debugger: true,
+        // drop_console: true
       }
     }),
+    //定义环境变量
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    }),
+    //生成HTML
     ...HtmlPlugin
   ],
-  devtool: isProduction ? null : 'source-map'
+  devtool: isProduction ? false : "source-map"
 };
